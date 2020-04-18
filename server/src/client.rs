c8b2ef7c3f207a58401f3ed643e7ee559d7a0dab
jdollar <dollarhidejoel@gmail.com>
2020-04-18 19:58:26 -0400

    Adding endpoint to trigger the relay

--- server/src/client.rs
+++ server/src/client.rs
@@ -1,7 +1,8 @@
 use tonic::Request;
 
 use garageopener::garage_opener_client::GarageOpenerClient;
-use garageopener::{Empty};
+use garageopener::{Empty, ChangeDoorStateRequest};
+use garageopener::change_door_state_request::Action;
 
 pub mod garageopener {
   tonic::include_proto!("garageopener");
@@ -16,6 +17,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .await?;
 
   println!("RESPONSE = {:?}", response);
+
+  let response2 = client
+    .change_door_state(Request::new(ChangeDoorStateRequest {
+      password: "1224".into(),
+      action: Action::Open as i32
+    }))
+    .await;
+
+  println!("RESPONSE2 = {:?}", response2);
+
+  let response3 = client
+    .change_door_state(Request::new(ChangeDoorStateRequest {
+      password: "1234".into(),
+      action: Action::Open as i32
+    }))
+    .await?;
+
+  println!("RESPONSE3 = {:?}", response3);
 
   Ok(())
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -2,64 +2,107 @@ use confy;
 use std::sync::Arc;
 use serde::{Serialize, Deserialize};
 use tonic::transport::Server;
-use tonic::{Request, Response, Status};
+use tonic::{Request, Response, Status, Code};
+use gpio_cdev::{Chip,LineRequestFlags,LineHandle};
+use std::thread::sleep;
+use std::time::Duration;
 
 use garageopener::garage_opener_server::{GarageOpener, GarageOpenerServer};
-use garageopener::{Empty, DoorState};
+use garageopener::{Empty, ChangeDoorStateRequest, ChangeDoorStateResponse, DoorState};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RelayConfig {
+    chip: String,
+    line: u32,
+    time_ms: u64
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct GarageOpenerConfig {
     port: u16,
-    gpiochip: String,
-    relay_line: u8,
-    relay_time_ms: usize,
+    password: String,
+    relay: RelayConfig
 }
 
 impl ::std::default::Default for GarageOpenerConfig {
     fn default() -> Self {
-      GarageOpenerConfig {
-        port: 10000,
-        gpiochip: "/dev/gpiochip0".into(),
-        relay_line: 17,
-        relay_time_ms: 1000
-      }
+        GarageOpenerConfig {
+            port: 10000,
+            password: "1234".into(),
+            relay: RelayConfig {
+                chip: "/dev/gpiochip0".into(),
+                line: 17,
+                time_ms: 1000
+            }
+        }
     }
 }
 
 pub mod garageopener {
-  tonic::include_proto!("garageopener");
+    tonic::include_proto!("garageopener");
 }
 
 #[derive(Debug)]
 pub struct GarageOpenerService {
-  cfg: Arc<GarageOpenerConfig>
+    cfg: Arc<GarageOpenerConfig>,
+    gpio_handle: Arc<LineHandle>
 }
 
 #[tonic::async_trait]
 impl GarageOpener for GarageOpenerService {
-  async fn get_garage_door_state(&self, _request: Request<Empty>) -> Result<Response<DoorState>, Status> {
-    dbg!(&self.cfg);
-    Ok(Response::new(DoorState::default()))
-  }
+    async fn get_garage_door_state(&self, _request: Request<Empty>) -> Result<Response<DoorState>, Status> {
+        dbg!(&self.cfg);
+        Ok(Response::new(DoorState::default()))
+    }
+
+    async fn change_door_state(&self, request: Request<ChangeDoorStateRequest>) -> Result<Response<ChangeDoorStateResponse>, Status> {
+        if request.into_inner().password != self.cfg.password {
+            return Err(Status::new(Code::Unauthenticated, "Password provided is incorrect"));
+        }
+
+        match self.gpio_handle.set_value(0) {
+            Err(_) => return Err(Status::new(Code::Internal, "Error turning the relay on")),
+            _ => ()
+        }
+
+        sleep(Duration::from_millis(self.cfg.relay.time_ms));
+
+        match self.gpio_handle.set_value(1) {
+            Err(_) => return Err(Status::new(Code::Internal, "Error turning the relay off")),
+            _ => ()
+        }
+
+        Ok(Response::new(ChangeDoorStateResponse {
+            success: true
+        }))
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cfg: GarageOpenerConfig = confy::load("garage-opener")?;
-
+    
     dbg!(&cfg);
 
+    // Setup relay
+    let mut chip = Chip::new(&cfg.relay.chip)?;
+
+    let handle = chip
+        .get_line(cfg.relay.line)?
+        .request(LineRequestFlags::OUTPUT, 1, "garage-switch")?;
+    
     let addr = format!("[::1]:{}", cfg.port).parse().unwrap();
-
+    
     println!("GarageDoorServer listening on: {}", addr);
-
+    
     let garage_opener = GarageOpenerService {
-      cfg: Arc::new(cfg),
+        cfg: Arc::new(cfg),
+        gpio_handle: Arc::new(handle),
     };
-
+    
     let svc = GarageOpenerServer::new(garage_opener);
-
+    
     Server::builder().add_service(svc).serve(addr).await?;
-
+    
     Ok(())
 }
